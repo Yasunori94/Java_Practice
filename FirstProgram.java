369dd79d2549de6f4ad12c8742929d0f7b5e1232
Yasunori94 <118075252+Yasunori94@users.noreply.github.com>
2023-02-25 20:17:27 +0900

    Initial addition

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+// I'm a comment. I have no impact on your code.
+// Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+class FirstProgram{
+    public static void main(String[] args) {
+        System.out.println("Y       Y");
+        System.out.println(" Y     Y");
+        System.out.println("  Y   Y");
+        System.out.println("    Y");
+    }
+}
