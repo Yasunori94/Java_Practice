7808fc7e356557896124448bd27fcb55ec2ba279
Yasunori94 <118075252+Yasunori94@users.noreply.github.com>
2023-02-26 21:50:28 +0900

    3

---
+++ Chorus.java
@@ -0,0 +1,14 @@
+public class Chorus {
+    public static void main(String[] args) {
+        
+        singChorus();
+
+    }
+
+    public static void singChorus (){
+        System.out.println("This is a lyrics");
+    }
+    
+}
+
+
